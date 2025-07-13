2578325fe1d2080f8a47f43b8b31ddadfd4df8fa
Amrillo <amrishka88@gmail.com>
2025-07-14 01:12:08 +0500

    fix: adjust configuration

--- class-components/vite.config.ts
+++ class-components/vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-    base: '/amrillo-REACT2025Q3/class-components/',
     plugins: [react()],
 });
