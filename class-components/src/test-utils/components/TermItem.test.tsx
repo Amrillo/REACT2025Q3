39e0b9099699899f5830878253f0e1383c53f07c
Amrillo <amrishka88@gmail.com>
2025-07-18 18:04:17 +0500

    feat: add tests for termitem component

--- class-components/src/test-utils/components/TermItem.test.tsx
+++ class-components/src/test-utils/components/TermItem.test.tsx
@@ -1,5 +1,5 @@
 import '@testing-library/jest-dom';
-import { fireEvent, render, screen } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { TermItem } from '../../components/ui/TermItem';
 
 describe('TermItem', () => { 
