743bbb42d4d8d032aadf4a0ccfbd181ce24c59c2
Amrillo <amrishka88@gmail.com>
2025-07-23 18:02:31 +0500

    feat: add basic router file

---
+++ hooks/src/routes/Router.tsx
@@ -0,0 +1,17 @@
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
+import Home from '../pages/Home';
+import About from '../pages/About';
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+]);
