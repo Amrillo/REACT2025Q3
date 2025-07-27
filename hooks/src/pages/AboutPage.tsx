1315cbda7dc6093b3cacc174f28c550b271459d9
Amrillo <amrishka88@gmail.com>
2025-07-28 00:00:00 +0500

    feat: add about author page

--- hooks/src/pages/AboutPage.tsx
+++ hooks/src/pages/AboutPage.tsx
@@ -1,14 +1,17 @@
-import type { FC } from "react"
+import type { FC } from 'react';
 
-export const AboutPage:FC = () => {  
-    return (
-        <section className="about">
-            <div className="container">
-                <h2 className="about-title">Amrillo Akhmedov</h2>
-                <p className="about-text">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, delectus fugit laborum modi earum magnam minus in sed libero, consequuntur, iste repudiandae nisi rem nemo reprehenderit animi eius cumque ipsum!
-                </p>
-            </div>
-        </section>
-    )
-} 
+export const AboutPage: FC = () => {
+  return (
+    <section className="about">
+      <div className="container">
+        <h2 className="about-title">Amrillo Akhmedov</h2>
+        <p className="about-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus,
+          delectus fugit laborum modi earum magnam minus in sed libero,
+          consequuntur, iste repudiandae nisi rem nemo reprehenderit animi eius
+          cumque ipsum!
+        </p>
+      </div>
+    </section>
+  );
+};
