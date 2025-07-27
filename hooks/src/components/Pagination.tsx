be92d62726de27ba88e5acfface1d88343b3f664
Amrillo <amrishka88@gmail.com>
2025-07-28 00:05:44 +0500

    refactor: upgrade the components

--- hooks/src/components/Pagination.tsx
+++ hooks/src/components/Pagination.tsx
@@ -1,23 +1,32 @@
-import { useState, type FC } from "react"
+import { type FC } from 'react';
 
-type PaginationPropsType = {  
-    setPage: (page: number) => void;
-    page : number
-}
-export const Pagination: FC<PaginationPropsType> = ({setPage , page}) => {  
-
-    return ( 
-        <div className="pagination">
-            <button
-                className="pagination-btn"
-                disabled = {page === 1}
-                onClick={()=> setPage(page - 1)}
-                > Prev </button>
-            <button className="pagination-page">{page}</button>
-            <button
-                className="pagination-btn"
-                onClick={()=> setPage(page + 1)}    
-                 >Next</button>
-        </div>
-    )
-}
+type PaginationPropsType = {
+  setPage: (page: number) => void;
+  page: number;
+  totalpages: number;
+};
+export const Pagination: FC<PaginationPropsType> = ({
+  setPage,
+  page,
+  totalpages,
+}) => {
+  return (
+    <div className="pagination">
+      <button
+        className="pagination-btn"
+        disabled={page === 1}
+        onClick={() => setPage(page - 1)}
+      >
+        Prev
+      </button>
+      <button className="pagination-page">{page}</button>
+      <button
+        className="pagination-btn"
+        onClick={() => setPage(page + 1)}
+        disabled={page === totalpages}
+      >
+        Next
+      </button>
+    </div>
+  );
+};
